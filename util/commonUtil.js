199df7235ee37a8471dcfc702af0c45f8b046dac
sedenowa <shiro.and.kai@gmail.com>
2017-01-07 17:21:09 +0900

    move removeCharacterWithPosition to commonUtil

---
+++ util/commonUtil.js
@@ -0,0 +1,10 @@
+//remove character with its potition
+//ex func("abc",1) -> return "ac"
+exports.removeCharacterWithPosition = function(string, index){
+	var firstHalf = string;
+	var latterHalf = string;
+	var stringRemovedChar = 
+		firstHalf.slice(0,index) + 
+		latterHalf.slice(index+1);
+	return stringRemovedChar;
+}
