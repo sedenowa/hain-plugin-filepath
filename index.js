69350b843d78417956d51c8988331f3e971c88f8
sedenowa <shiro.and.kai@gmail.com>
2017-01-09 06:44:03 +0900

    same

--- index.js
+++ index.js
@@ -5,48 +5,12 @@ module.exports = (pluginContext) => {
 	const app = pluginContext.app;
 	//to open File/Folder
 	const shell = pluginContext.shell;
-	//to access filesystem
-	const fs = require('fs');
-	//to format filepath
-	const path = require('path');
 	
 	//Utils
-	var commonSearchUtil = require("./util/commonSearchUtil");
 	var formatStringUtil = require("./util/formatStringUtil");
 	var searchPathUtil = require("./util/searchPathUtil");
 	var searchDriveUtil = require("./util/searchDriveUtil");
 	var complementPathUtil = require("./util/complementPathUtil");
-	
-	//check if the file or folder exists
-	//return 1:File 2:Folder -1,0:Invalid path
-	function checkFileOrFolder(path) {
-		try {
-			var stat = fs.statSync(path);
-			if(stat.isFile() == true){
-				return 1;//file
-			}else if(stat.isDirectory() == true){
-				return 2;//folder
-			}else{//unreachable
-				return 0;//invalid
-			}
-		} catch(err) {
-			if(err.code === 'ENOENT'){
-				return -1;//invalid
-			}
-		}
-	}
-
-	function checkFileServer(query){
-		//check if the path is file server.
-		//if the path is file server, remove "\\" attached on head.
-		var isFileServer = false;
-		if(query.indexOf("\\\\") == 0 && 
-		   query[("\\\\").length] != '\\' && 
-		   query[("\\\\").length] != undefined){
-			isFileServer = true;
-		}
-		return isFileServer;
-	}
 	
 	function startup(){
 		//Search Available Drives.
@@ -56,12 +20,13 @@ module.exports = (pluginContext) => {
 	function search (query, res) {
 		//format query.
 		var formattedQuery = formatStringUtil.formatString(query);
-		
+
 		//search available path considering unnecessary spaces.
 		searchPathUtil.addOpenCommand(formattedQuery, res);
-		
+
 		//search available path to complement
-		complementPathUtil.searchCandidates(formattedQuery, searchDriveUtil.getAvailableDrives(), res);
+		var availableDrives = searchDriveUtil.getAvailableDrives();
+		complementPathUtil.searchCandidates(formattedQuery, availableDrives, res);
 
 		//refresh command to the end of list
 		searchDriveUtil.addRefreshCommand(query,res);
